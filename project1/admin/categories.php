f82c72ddf170aa88f5e86e3990960b8171a9cf9e
Sudip Parajuli <sudip.parajulee@gmail.com>
2023-12-08 11:11:14 +0545

    dashboard and categories

---
+++ project1/admin/categories.php
@@ -0,0 +1,23 @@
+<?php include('header.php'); ?>
+        <h2 class="text-3xl font-bold">Categories</h2>
+        <hr class="h-1 bg-blue-500">
+
+        <table class="mt-5 w-full">
+            <tr class="bg-gray-200 text-left">
+                <th class="p-3 border border-gray-50">S.N.</th>
+                <th class="p-3 border border-gray-50">Category Name</th>
+                <th class="p-3 border border-gray-50">Action</th>
+            </tr>
+            
+            <tr>
+                <td class="p-3 border border-gray-200">1</td>
+                <td class="p-3 border border-gray-200">National</td>
+                <td class="p-3 border border-gray-200">
+                    <a href="" class="bg-blue-600 text-white px-2 py-1 rounded">Edit</a>
+                    <a href="" class="bg-red-600 text-white px-2 py-1 rounded">Delete</a>
+                </td>
+            </tr>
+            
+        </table>
+
+<?php include('footer.php'); ?>
--- project1/admin/dashboard.php
+++ project1/admin/dashboard.php
@@ -1,21 +1,5 @@
-<html>
-    <head>
-        <title>Admin Panel</title>
-        <script src="https://cdn.tailwindcss.com"></script>
-    </head>
-    <body>
-        <div class="flex">
-            <div class="w-52 bg-gray-200 shadow h-screen">
-                <img src="https://www.bitmapitsolution.com/images/logo/logo.png" alt="" class="bg-white w-3/4 mx-auto mt-4 rounded-lg py-2">
-                <div class="mt-4 pl-2">
-                    <a href="" class="block border-l-4 border-blue-400 pl-2 text-xl py-2 mt-4 hover:bg-gray-300">Dashboard</a>
-                    <a href="" class="block border-l-4 border-blue-400 pl-2 text-xl py-2 mt-4 hover:bg-gray-300">Categories</a>
-                    <a href="" class="block border-l-4 border-blue-400 pl-2 text-xl py-2 mt-4 hover:bg-gray-300">Users</a>
-                    <a href="" class="block border-l-4 border-blue-400 pl-2 text-xl py-2 mt-4 hover:bg-gray-300">News</a>
-                    <a href="" class="block border-l-4 border-blue-400 pl-2 text-xl py-2 mt-4 hover:bg-gray-300">Logout</a>
-                </div>
-            </div>
-            <div class="flex-1 p-4">
+<?php include('header.php'); ?>
+            
                 <h2 class="text-3xl font-bold">Dashboard</h2>
                 <hr class="h-1 bg-blue-500">
                 <div class="grid grid-cols-3 gap-4 mt-4">
@@ -34,7 +18,5 @@
                         <h2 class="text-4xl font-bold">55</h2>
                     </div>
                 </div>
-            </div>
-        </div>
-    </body>
-</html>
+           
+<?php include('footer.php'); ?>
